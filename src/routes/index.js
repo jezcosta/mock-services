f5d5c347f8a5fab4530cd88405e5b9475a127272
Jessica Costa <jessicacosta.dev@hotmail.com>
2023-06-21 17:00:58 -0300

    fix(routes): remove delay

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,14 +10,14 @@ const RouterHandler = (req, res, next) => {
         res.status(response.status);
         res.set(response.headers);
         res.send(response.data || {});
-      }, response.delay || 200)
+      }, 200)
     })
     .catch((error) => {
       setTimeout(() => {
         res.status(error.status || 500);
         res.set(error.headers);
         res.send(error.data || {});
-      }, response.delay || 200)
+      }, 200)
     });
 };
 
